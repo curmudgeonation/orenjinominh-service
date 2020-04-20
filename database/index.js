8688a29924d11d0c5c4d24a65e40eaa50c7c250d
Minh <minhvn29@gmail.com>
2020-04-20 16:18:06 -0700

    Create .env file and update vars

--- database/index.js
+++ database/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost:27017/similarProperties';
+const mongoUri = process.env.MONGODB_URI;
 
-const db = mongoose.connect(mongoUri);
+const db = mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 module.exports = db;
