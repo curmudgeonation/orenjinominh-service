74331169674d3a91ec0254770d9fb382fff850ff
Minh <minhvn29@gmail.com>
2020-05-12 21:31:31 -0700

    Refactor star to xsm size

--- client/src/components/styledComponents.jsx
+++ client/src/components/styledComponents.jsx
@@ -17,18 +17,18 @@ object-fit: cover;
 
 export const Details = styled.div`
 font-weight: 400 !important;
-font-size: 16px !important;
+font-size: 20px !important;
 line-height: 20px !important;
 `
 
 export const SleepArrangement = styled.span`
-  font-size: 14px;
+  font-size: 16px;
   display: block;
   float: left;
   color: #808080;
 `
 export const Star = styled.span`
-  font-size: 14px;
+  font-size: 16px;
   display: block;
   float: right;
 `
