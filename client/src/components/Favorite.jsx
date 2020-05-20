f0a2cbc4ad4e4dffc2ab341c053e690f50bbeba8
Minh <minhvn29@gmail.com>
2020-05-19 21:51:03 -0700

    Minor Refactor to Favorite component; Change npm run build to production mode

--- client/src/components/Favorite.jsx
+++ client/src/components/Favorite.jsx
@@ -7,7 +7,9 @@ class Favorite extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Favorited: 'https://minhngo-fec-photos.s3-us-west-1.amazonaws.com/notFav.png'
+      NotFavoritedPNG: 'https://minhngo-fec-photos.s3-us-west-1.amazonaws.com/notFav.png',
+      FavoritedPNG: 'https://minhngo-fec-photos.s3-us-west-1.amazonaws.com/Fave.png',
+      saved: false,
     }
 
     this.ToggleSave = this.ToggleSave.bind(this);
@@ -15,16 +17,22 @@ class Favorite extends React.Component {
   }
 
   ToggleSave() {
-    if (this.state.Favorited === 'https://minhngo-fec-photos.s3-us-west-1.amazonaws.com/notFav.png') {
-      this.setState({Favorited:'https://minhngo-fec-photos.s3-us-west-1.amazonaws.com/Fave.png'});
+    if (this.state.saved === false) {
+      this.setState({saved: true});
     } else {
-      this.setState({Favorited:'https://minhngo-fec-photos.s3-us-west-1.amazonaws.com/Fave.png'});
+      this.setState({saved: false})
     }
   }
 
   render() {
     return (
-      <FavoriteIcon onClick={this.ToggleSave}>{<img src={this.state.Favorited}/>}</FavoriteIcon>
+      <FavoriteIcon onClick={this.ToggleSave}>
+        {this.state.saved ?
+          <img src={this.state.FavoritedPNG}/>
+          :
+          <img src={this.state.NotFavoritedPNG}/>
+        }
+      </FavoriteIcon>
     );
   }
 
