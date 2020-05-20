5581ba3d3422ac6982ddb838868d7d68f77ed406
Minh <minhvn29@gmail.com>
2020-05-20 16:09:20 -0700

    Configure port for EC2

--- server/start.js
+++ server/start.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const app = require('./index.js');
 
-const port = process.env.PORT || 2000;
+const port = process.env.PORT;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
